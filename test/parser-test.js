9874884c9285291ec5273fe9c78184b376b2c77d
Allison Davis <a.rae.davis@gmail.com>
2016-05-24 15:48:59 -0700

    end2end tests passing

--- test/parser-test.js
+++ test/parser-test.js
@@ -1,16 +1,33 @@
-// const app = require('../app');
-// const chai = require('chai');
-// const chaiHttp = require('chai-http');
-// const assert = chai.assert;
-// chai.use(chaiHttp);
-//
-// describe('parser middleware', () => {
-//   it('parses and returns a json object', (done) =>{
-//
-//   });
-//
-//   it('if invalid JSON returns err', (done) =>{
-//
-//   });
-//
-// });
+const app = require('../app');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const assert = chai.assert;
+chai.use(chaiHttp);
+
+var testData = {name: 'Skeeve Holt!', number: 49, position: 'blocker', captain: true };
+
+
+describe('parser middleware', () => {
+  var request = chai.request(app);
+
+  it('parses and returns a json object', (done) =>{
+    request
+      .post('/')
+      .send(testData)
+      .end((res) => {
+        assert.equal(res.body, testData);
+      });
+    done();
+  });
+
+  it('if invalid JSON returns err', (done) =>{
+    request
+      .post('/')
+      .send(0)
+      .end((res) => {
+        assert.equal(res.status, 400);
+      });
+    done();
+  });
+
+});
