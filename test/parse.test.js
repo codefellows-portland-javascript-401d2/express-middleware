f28a697e22e0dc826681cb15072067bc1f46d260
Yvonne Hayes <queyvonne@gmail.com>
2016-05-24 14:34:05 -0700

    got first test working. Checking if data is object and if body returns correctly

---
+++ test/parse.test.js
@@ -0,0 +1,34 @@
+const chai = require( 'chai' );
+const chaiHttp = require ( 'chai-http' );
+chai.use(chaiHttp);
+const assert = chai.assert;
+const request = chai.request;
+const expect = chai.expect;
+const server = require( '../index.js');
+
+var req;
+var error;
+
+describe ( 'Body Parser Middleware', () => {
+
+  describe('Parsing the json object', () => {
+
+    it('data should get saved as json object to req.body', () => {
+      req = {body: '', data: '{"pet": "dog"}'};
+      function parse(obj){
+        obj.body = JSON.parse(obj.data);
+        if(!(typeof req.body === 'object')) {
+          error = '400 Invalid JSON';
+        }
+      }
+      parse(req);
+      expect(req.body).to.be.an('object');
+      expect(req.body).to.eql({pet: 'dog'});
+      // assert.equal(req.body, ({pet: 'dog'}));
+    });
+
+  });
+
+
+
+});
